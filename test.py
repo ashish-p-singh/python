79f0b74f15a62882a4e7430c79a9f039682c9d29
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-03-29 15:35:54 +0530

    Refactor test.py to enable input processing and remove commented-out code

--- test.py
+++ test.py
@@ -1,26 +1,19 @@
-# a=int(input())
-# l=[]
+a=int(input())
+l=[]
 
-# for k in range(a):
-#     s,r,c= map(int,input().split())
-#     arr=list(map(int,input().split()))
-#     arr.sort()
-#     i=0
-#     temp=0
-#     while(i<s-1):
-#         if(arr[i]<=r and arr[i+1]<=c):
-#             temp+=1
-#             i+=2
-#         else:
-#             i+=1  
-#     l.append(temp)
-# for x in l:
-#     print(x)
+for k in range(a):
+    s,r,c= map(int,input().split())
+    arr=list(map(int,input().split()))
+
+for x in l:
+    print(x)
 #--------------------------------------
-n=int(input())
-a=str(n)
-b=a[::-1]
-if(a==b):
-    print("true")
-else:
-    print("false")
+# n=int(input())
+# a=str(n)
+# b=a[::-1]
+# if(a==b):
+#     print("true")
+# else:
+#     print("false")
+#------------------------------------------------------
+a=input()
