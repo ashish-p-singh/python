0b51d5f1c7a1545e4346cac8f9ff57472d7f5c7d
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-04-01 00:47:23 +0530

    Refactor test.py to remove unused code and implement new string and number processing functionalities

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-# a=int(input())
-# l=[]
-
-# for k in range(a):
-#     s,r,c= map(int,input().split())
-#     arr=list(map(int,input().split()))
-
-# for x in l:
-#     print(x)
 # --------------------------------------
 # n=int(input())
 # a=str(n)
@@ -16,3 +7,137 @@
 # else:
 #     print("false")
 # ---------------------------------------------------
+# strs=input().split(",")
+# t=[]
+
+# b=strs[0]
+# for j in range(len(strs)-1):
+#     u=[]
+#     c=strs[j+1]
+#     for k in range(min(len(b),len(c))):
+#         if (j==0):
+#             if b[k]==c[k]:
+#                 t.append(k)
+#             else:
+#                 break
+#         else:
+#             if b[k]==c[k]:
+#                 if k in t:
+#                     u.append(k)
+#                 else:
+#                     break
+#             else: 
+#                 break
+#     if j!=0:
+#         t=u.copy()
+
+# if len(strs)==1:
+#     print(b)
+# elif u:
+#     d=u[-1]+1
+#     print(b[0:d])
+# else:
+#     print("")
+#----------------------------------------------------------
+# s=input().strip()
+# t=[]
+# for i in range(len(s)):
+#         if(s[i]=="("):
+#             t.append("(")
+#         elif(s[i]=="{"):
+#             t.append("{")
+#         elif(s[i]=="["):
+#             t.append("[")
+#         elif(s[i]==")"):
+#             if t:
+#                 if t[-1]=="(":
+#                     t.pop()
+#                 else:
+#                     print("false")
+#                     exit()
+#             else:
+#                 print("false")
+#                 exit()
+#         elif(s[i]=="}"):
+#             if t:
+#                 if t[-1]=="{":
+#                     t.pop()
+#                 else:
+#                     print("false")
+#                     exit()
+#             else:
+#                 print("false")
+#                 exit()  
+#         elif(s[i]=="]"):
+#             if t:
+#                 if t[-1]=="[":
+#                     t.pop()
+#                 else:
+#                     print("false")
+#                     exit()
+#             else:
+#                 print("false")      
+#                 exit()    
+# if not t:
+#     print ("true")
+# else:
+#     print("false")
+#------------------------------------------------------------------
+# nums=list(map(int,input().split()))
+
+# for i in range(len(nums)):
+#     t=[0]
+#     for j in range(len(nums)):
+#         if nums[i]==nums[j]:
+#             t[0]+=1
+#     if t[0]==1:
+#         print(nums[i])
+#         break
+# s=input()
+# roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+# t=0
+# p=0
+# for x in s[::-1]:
+#     l=roman[x]
+
+#     if l < p:
+#             t-=l
+#             p=l
+#     else:
+#             t+=l
+#             p=l
+# print(t)
+#--------------------------------------------
+# nums=list(map(int,input().split()))
+# b=len(nums)
+# for i in range(len(nums)+1):
+#     if i not in nums:
+#         print(i)
+#----------------------------------------------
+haystack=input()
+needle=input()
+if not needle:
+    print(0)
+a=needle[0]
+b=None
+for i in range(len(haystack)):
+    if haystack[i]==a:
+        b=i
+        k=i
+        for j in range(len(needle)):
+            if k>len(haystack)-1:
+                b=None
+                break
+            if(haystack[k]!=needle[j]):
+                  b=None
+                  break
+            else:
+                 k+=1
+        else:
+            break
+
+if b==None:
+    print(-1)
+else:
+    print(b)
+    
